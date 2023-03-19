58b96c61020a776f41e1757373512c8a0942d86e
rudzzz <rudsolza13@gmail.com>
2023-03-19 11:59:26 -0300

    LinkedLists code

---
+++ src/LinkedLists/Main.java
@@ -0,0 +1,11 @@
+package LinkedLists;
+
+public class Main {
+   public static void main(String[] args){
+       LinkedList myLinkedList = new LinkedList(1);
+
+
+       myLinkedList.append(2);
+       myLinkedList.printList();
+   }
+}
