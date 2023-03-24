d134c4fb54aaa426b159dfb16fd2851647e3ca02
rudzzz <rudsolza13@gmail.com>
2023-03-24 20:06:19 -0300

    call to removeLast method

--- src/LinkedLists/Main.java
+++ src/LinkedLists/Main.java
@@ -4,8 +4,11 @@ public class Main {
    public static void main(String[] args){
        LinkedList myLinkedList = new LinkedList(1);
 
-
        myLinkedList.append(2);
-       myLinkedList.printList();
+       //myLinkedList.printList();
+
+       System.out.println(myLinkedList.removeLast().value);
+       System.out.println(myLinkedList.removeLast().value);
+       System.out.println(myLinkedList.removeLast());
    }
 }
