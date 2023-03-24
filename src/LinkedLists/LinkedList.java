f0d91830ae003df53b9fdd73e3677070501fdfa5
rudzzz <rudsolza13@gmail.com>
2023-03-24 20:05:33 -0300

    removeLast method added

--- src/LinkedLists/LinkedList.java
+++ src/LinkedLists/LinkedList.java
@@ -42,4 +42,28 @@ public class LinkedList {
         }
         length++;
     }
+
+    public Node removeLast(){
+        if(length == 0){
+            return null;
+        }
+        Node temp = head;
+        Node pre = head;
+
+        while(temp.next != null){
+            pre = temp;
+            temp = temp.next;
+        }
+
+        tail = pre;
+        tail.next = null;
+        length--;
+
+        if(length == 0){
+            head = null;
+            tail = null;
+        }
+
+        return temp;
+    }
 }
