620dcff3372b7259fa4f8fd32d5fd53b902678ac
rudzzz <rudsolza13@gmail.com>
2023-03-25 18:13:29 -0300

    call to get, set and insert methods

--- src/LinkedLists/Main.java
+++ src/LinkedLists/Main.java
@@ -19,10 +19,37 @@ public class Main {
        //myLinkedList.printList();
 
        //remove first
-      LinkedList myLinkedList = new LinkedList(2);
-      myLinkedList.append(1);
-      System.out.println(myLinkedList.removeFirst().value);
-      System.out.println(myLinkedList.removeFirst().value);
-      System.out.println(myLinkedList.removeFirst());
+      //LinkedList myLinkedList = new LinkedList(2);
+      //myLinkedList.append(1);
+      //System.out.println(myLinkedList.removeFirst().value);
+      //System.out.println(myLinkedList.removeFirst().value);
+      //System.out.println(myLinkedList.removeFirst());
+
+      //get index
+      //LinkedList myLinkedList = new LinkedList(0);
+      //myLinkedList.append(1);
+      //myLinkedList.append(2);
+      //myLinkedList.append(3);
+
+      //System.out.println(myLinkedList.get(2).value + "\n");
+      //myLinkedList.printList();
+
+      //set
+      //LinkedList myLinkedList = new LinkedList(11);
+      //myLinkedList.append(3);
+      //myLinkedList.append(23);
+      //myLinkedList.append(7);
+      //myLinkedList.printList();
+
+      //myLinkedList.set(2, 4);
+      //myLinkedList.printList();
+
+      //insert
+      LinkedList myLinkedList = new LinkedList(0);
+      myLinkedList.append(2);
+      myLinkedList.printList();
+
+      myLinkedList.set(0, 1);
+      myLinkedList.printList();
    }
 }
