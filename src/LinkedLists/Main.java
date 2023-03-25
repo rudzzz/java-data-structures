b134ae050fde8e3d9618706b811a978953b97d80
rudzzz <rudsolza13@gmail.com>
2023-03-25 16:29:20 -0300

    call to prepend and removeFirst method

--- src/LinkedLists/Main.java
+++ src/LinkedLists/Main.java
@@ -2,13 +2,27 @@ package LinkedLists;
 
 public class Main {
    public static void main(String[] args){
-       LinkedList myLinkedList = new LinkedList(1);
-
-       myLinkedList.append(2);
+        //append
+       //LinkedList myLinkedList = new LinkedList(1);
+       //myLinkedList.append(2);
        //myLinkedList.printList();
 
-       System.out.println(myLinkedList.removeLast().value);
-       System.out.println(myLinkedList.removeLast().value);
-       System.out.println(myLinkedList.removeLast());
+       //remove
+       //System.out.println(myLinkedList.removeLast().value);
+       //System.out.println(myLinkedList.removeLast().value);
+       //System.out.println(myLinkedList.removeLast());
+
+       //prepend
+       //LinkedList myLinkedList = new LinkedList(2);
+       //myLinkedList.append(3);
+       //myLinkedList.prepend(1);
+       //myLinkedList.printList();
+
+       //remove first
+      LinkedList myLinkedList = new LinkedList(2);
+      myLinkedList.append(1);
+      System.out.println(myLinkedList.removeFirst().value);
+      System.out.println(myLinkedList.removeFirst().value);
+      System.out.println(myLinkedList.removeFirst());
    }
 }
