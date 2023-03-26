11a9ab4e27361795f8ae9c95725caa293b343aea
rudzzz <rudsolza13@gmail.com>
2023-03-26 08:10:59 -0300

    Main class added

---
+++ src/DoublyLinkedLists/Main.java
@@ -0,0 +1,13 @@
+package DoublyLinkedLists;
+
+public class Main {
+    public static void main(String[] args){
+        DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(7);
+
+        myDoublyLinkedList.getHead();
+        myDoublyLinkedList.getTail();
+        myDoublyLinkedList.getLength();
+
+        myDoublyLinkedList.printList();
+    }
+}
