a675f09ec5883ea0e4f334656430e9bfd021941e
rudzzz <rudsolza13@gmail.com>
2023-03-27 21:07:05 -0300

    call to append method

--- src/DoublyLinkedLists/Main.java
+++ src/DoublyLinkedLists/Main.java
@@ -2,12 +2,17 @@ package DoublyLinkedLists;
 
 public class Main {
     public static void main(String[] args){
-        DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(7);
+        //DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(7);
 
-        myDoublyLinkedList.getHead();
-        myDoublyLinkedList.getTail();
-        myDoublyLinkedList.getLength();
+        //myDoublyLinkedList.getHead();
+        //myDoublyLinkedList.getTail();
+        //myDoublyLinkedList.getLength();
 
+        //myDoublyLinkedList.printList();
+
+        //append
+        DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(1);
+        myDoublyLinkedList.append(2);
         myDoublyLinkedList.printList();
     }
 }
