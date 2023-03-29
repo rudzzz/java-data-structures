37a61f5de7532fa3e3e93965e163555eea553cc9
rudzzz <rudsolza13@gmail.com>
2023-03-29 20:25:44 -0300

    call to insert method

--- src/DoublyLinkedLists/Main.java
+++ src/DoublyLinkedLists/Main.java
@@ -16,14 +16,51 @@ public class Main {
         //myDoublyLinkedList.printList();
 
         //removelast
+        //DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(1);
+        //myDoublyLinkedList.append(2);
+        //myDoublyLinkedList.append(3);
+        //myDoublyLinkedList.printList();
+        //System.out.println();
+        //System.out.println(myDoublyLinkedList.removeLast().value);
+        //System.out.println(myDoublyLinkedList.removeLast().value);
+        //System.out.println(myDoublyLinkedList.removeLast().value);
+        //myDoublyLinkedList.printList();
+
+        //prepend
+        //DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(2);
+        //myDoublyLinkedList.append(3);
+        //myDoublyLinkedList.prepend(1);
+        //myDoublyLinkedList.printList();
+
+        //removeFirst
+        //DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(1);
+        //myDoublyLinkedList.append(2);
+        //myDoublyLinkedList.append(3);
+        //myDoublyLinkedList.removeFirst();
+        //myDoublyLinkedList.printList();
+
+        //get
+        //DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(0);
+        //myDoublyLinkedList.append(1);
+        //myDoublyLinkedList.append(2);
+        //myDoublyLinkedList.append(3);
+        //System.out.println(myDoublyLinkedList.get(1).value);
+        //System.out.println(myDoublyLinkedList.get(2).value);
+
+        //set
+        //DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(0);
+        //myDoublyLinkedList.append(1);
+        //myDoublyLinkedList.append(2);
+        //myDoublyLinkedList.append(3);
+        //System.out.println(myDoublyLinkedList.set(2, 3));
+        //myDoublyLinkedList.printList();
+
+        //insert
         DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(1);
-        myDoublyLinkedList.append(2);
         myDoublyLinkedList.append(3);
         myDoublyLinkedList.printList();
         System.out.println();
-        System.out.println(myDoublyLinkedList.removeLast().value);
-        System.out.println(myDoublyLinkedList.removeLast().value);
-        System.out.println(myDoublyLinkedList.removeLast().value);
+        myDoublyLinkedList.insert(1, 2);
         myDoublyLinkedList.printList();
     }
 }
