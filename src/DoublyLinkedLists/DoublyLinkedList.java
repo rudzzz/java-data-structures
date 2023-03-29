8f1d4190871e7dd1a45c0e80c0c32e5ba821b40d
rudzzz <rudsolza13@gmail.com>
2023-03-29 20:26:04 -0300

    insert method added

--- src/DoublyLinkedLists/DoublyLinkedList.java
+++ src/DoublyLinkedLists/DoublyLinkedList.java
@@ -64,4 +64,87 @@ public class DoublyLinkedList {
         length--;
         return temp;
     }
+
+    public void prepend(int value){
+        Node newNode = new Node(value);
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst(){
+        if(length == 0){
+            return null;
+        }
+        Node temp = head;
+        if(length == 1){
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public Node get(int index){
+        if(length == 0 || index >= length){
+            return null;
+        }
+        Node temp = head;
+        if(index < length / 2){
+            for(int i = 0; i < index; i++){
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for(int i = length - 1; i > index; i--){
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value){
+        Node temp = get(index);
+        if(temp != null){
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value){
+        if(index < 0 || index > length){
+            return false;
+        }
+        if(index == 0){
+            prepend(value);
+            return true;
+        }
+        if(index == length){
+            append(value);
+            return true;
+        }
+
+        Node newNode = new Node(value);
+        Node before = get(index - 1);
+        Node after = before.next;
+
+        newNode.prev = before;
+        newNode.next = after;
+        before.next = newNode;
+        after.prev = newNode;
+        length++;
+
+        return true;
+    }
 }
