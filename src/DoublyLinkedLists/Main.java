823619961cdb85e8c03695bec38f1c0e0524f01e
rudzzz <rudsolza13@gmail.com>
2023-03-29 19:45:58 -0300

    call to removeLast method

--- src/DoublyLinkedLists/Main.java
+++ src/DoublyLinkedLists/Main.java
@@ -11,8 +11,19 @@ public class Main {
         //myDoublyLinkedList.printList();
 
         //append
+        //DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(1);
+        //myDoublyLinkedList.append(2);
+        //myDoublyLinkedList.printList();
+
+        //removelast
         DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(1);
         myDoublyLinkedList.append(2);
+        myDoublyLinkedList.append(3);
+        myDoublyLinkedList.printList();
+        System.out.println();
+        System.out.println(myDoublyLinkedList.removeLast().value);
+        System.out.println(myDoublyLinkedList.removeLast().value);
+        System.out.println(myDoublyLinkedList.removeLast().value);
         myDoublyLinkedList.printList();
     }
 }
