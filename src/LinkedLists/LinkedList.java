0b4084d658690722105c6479a66f89fe4ccf299d
rudzzz <rudsolza13@gmail.com>
2023-04-01 11:12:17 -0300

    findMiddleNode method added

--- src/LinkedLists/LinkedList.java
+++ src/LinkedLists/LinkedList.java
@@ -166,4 +166,15 @@ public class LinkedList {
             temp = after;
         }
     }
+
+    public Node findMiddleNode(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 }
