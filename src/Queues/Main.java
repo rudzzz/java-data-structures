f57cfff53382c1e959e98b78b8d4305f85cd4b1d
rudzzz <rudsolza13@gmail.com>
2023-04-03 21:39:33 -0300

    call to methods

---
+++ src/Queues/Main.java
@@ -0,0 +1,37 @@
+package Queues;
+
+public class Main {
+    public static void main(String[] args){
+        //create queue
+        //Queue myQueue = new Queue(7);
+        //myQueue.getFirst();
+        //myQueue.getLast();
+        //myQueue.getLength();
+        //myQueue.printQueue();
+
+       /* Enqueue
+        Queue myQueue = new Queue(1);
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+        myQueue.printQueue();
+
+        myQueue.enqueue(2);
+        System.out.println();
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+        myQueue.printQueue();
+        */
+
+     /* Denqueue
+        Queue myQueue = new Queue(1);
+        myQueue.enqueue(2);
+
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue());
+
+      */
+    }
+}
