83a09e6c2d3068ee5d1a140680512aa6ab2d8aae
rudzzz <rudsolza13@gmail.com>
2023-04-07 14:17:06 -0300

    call to insert method

--- src/BinarySearchTrees/Main.java
+++ src/BinarySearchTrees/Main.java
@@ -2,7 +2,23 @@ package BinarySearchTrees;
 
 public class Main {
     public static void main(String[] args){
+        /*
         BinarySearchTree myBinarySearchTree = new BinarySearchTree();
         System.out.println("Root = " + myBinarySearchTree.root);
+        */
+
+        // insert
+        BinarySearchTree myBinarySearchTree = new BinarySearchTree();
+
+        myBinarySearchTree.insert(47);
+        myBinarySearchTree.insert(21);
+        myBinarySearchTree.insert(76);
+        myBinarySearchTree.insert(18);
+        myBinarySearchTree.insert(52);
+        myBinarySearchTree.insert(82);
+
+        myBinarySearchTree.insert(27);
+
+        System.out.println(myBinarySearchTree.root.left.right.value);
     }
 }
