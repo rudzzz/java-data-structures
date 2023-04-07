d065fa2e700dac1b9a8344414975110852a60e11
rudzzz <rudsolza13@gmail.com>
2023-04-07 13:42:43 -0300

    Main class added

---
+++ src/BinarySearchTrees/Main.java
@@ -0,0 +1,8 @@
+package BinarySearchTrees;
+
+public class Main {
+    public static void main(String[] args){
+        BinarySearchTree myBinarySearchTree = new BinarySearchTree();
+        System.out.println("Root = " + myBinarySearchTree.root);
+    }
+}
