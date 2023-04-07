287ef8da9cecdad4afe8feb0e18e63998dea5772
rudzzz <rudsolza13@gmail.com>
2023-04-07 14:17:25 -0300

    insert method added

--- src/BinarySearchTrees/BinarySearchTree.java
+++ src/BinarySearchTrees/BinarySearchTree.java
@@ -2,4 +2,33 @@ package BinarySearchTrees;
 
 public class BinarySearchTree {
     Node root;
+
+    public boolean insert(int value){
+        Node newNode = new Node(value);
+        if(root == null){
+            root = newNode;
+            return true;
+        }
+
+        Node temp = root;
+
+        while(true){
+            if(newNode.value == temp.value){
+                return false;
+            }
+            if(newNode.value < temp.value){
+                if(temp.left == null){
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if(temp.right == null){
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
 }
